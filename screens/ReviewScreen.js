9524682866b360ed55d4315763d3bd71cdc4fb94
Jordan <administrator@JTGs-MBP.fios-router.home>
2017-10-18 00:47:29 -0400

    Setting + tabs wired up

---
+++ screens/ReviewScreen.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { View, Text, Button } from 'react-native';
+
+class ReviewScreen extends Component {
+  static navigationOptions = ({ navigation }) => {
+      return {
+        title: 'Review Jobs',
+        headerRight: (<Button title='Settings' onPress={() => navigation.navigate('settings')} />
+      )
+    };
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>ReviewScreen</Text>
+        <Text>ReviewScreen</Text>
+        <Text>ReviewScreen</Text>
+        <Text>ReviewScreen</Text>
+        <Text>ReviewScreen</Text>
+        <Text>ReviewScreen</Text>
+      </View>
+    );
+  }
+}
+
+export default ReviewScreen;
