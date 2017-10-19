5caeb2f13b2325e2423beb201aeb90c9e20f1786
Jordan <administrator@JTGs-MBP.fios-router.home>
2017-10-19 16:25:28 -0400

    map & review wired up, review not adding in all cards

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
 import { MapView } from "expo";
+import { connect } from 'react-redux';
+import { Button } from 'react-native-elements';
+
+import * as actions from '../actions';
 
 class MapScreen extends Component {
   state = {
@@ -17,6 +21,12 @@ class MapScreen extends Component {
     this.setState({ region });
   }
 
+  onButtonPress = () => {
+    this.props.fetchJobs(this.state.region, () => {
+      this.props.navigation.navigate('deck');
+    });
+  }
+
   render() {
     return (
       <View style={{ flex: 1 }}>
@@ -25,9 +35,26 @@ class MapScreen extends Component {
           style={{ flex: 1 }}
           onRegionChangeComplete={this.onRegionChangeComplete}
         />
+        <View style={styles.buttonContainer}>
+          <Button
+            large
+            title="Search This Area"
+            backgroundColor='rgb(0, 150, 136)'
+            icon={{ name: 'search' }}
+            onPress={this.onButtonPress} //would normally have a ./bind(this)
+          />
+        </View>
       </View>
     );
   }
 }
 
-export default MapScreen;
+const styles = {
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 20,
+    left: 0
+  }
+}
+
+export default connect(null, actions)(MapScreen);
