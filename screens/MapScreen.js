b37310b4bca3c5b672f8fb1da58d05e7a49ffe0b
Jordan <administrator@JTGs-MBP.fios-router.home>
2017-10-20 16:33:51 -0400

    getting icons to show up

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -7,6 +7,9 @@ import { Button } from 'react-native-elements';
 import * as actions from '../actions';
 
 class MapScreen extends Component {
+  static navigationOptions = {
+    title: 'Map'
+  }
   state = {
     mapLoaded: false,
     region: {
